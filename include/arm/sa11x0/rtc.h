48a9d41049533fad3f3ecc542078938b788302bc
Marcel Telka <marcel@telka.sk>
2002-06-27 13:02:03 +0000

    Added Real-Time Clock Registers.
    git-svn-id: https://urjtag.svn.sourceforge.net/svnroot/urjtag/trunk@27 b68d4a1b-bc3d-0410-92ed-d4ac073336b7

---
+++ include/arm/sa11x0/rtc.h
@@ -0,0 +1,60 @@
+/*
+ * $Id$
+ *
+ * StrongARM SA-1110 Real-Time Clock Registers
+ * Copyright (C) 2002 ETC s.r.o.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by Marcel Telka <marcel@telka.sk>, 2002.
+ *
+ * Documentation:
+ * [1] Intel Corporation, "Intel StrongARM SA-1110 Microprocessor
+ *     Developer's Manual", October 2001, Order Number: 278240-004
+ * [2] Intel Corporation, "Intel StrongARM SA-1110 Microprocessor
+ *     Specification Update", December 2001, Order Number: 278259-023
+ *
+ */
+
+#ifndef	SA11X0_RTC_H
+#define	SA11X0_RTC_H
+
+#ifndef uint32_t
+typedef	unsigned int	uint32_t;
+#endif
+
+/* Real-Time Clock Registers */
+
+#define	RTC_BASE	0x90010000
+
+typedef volatile struct RTC_registers {
+	uint32_t rtar;
+	uint32_t rcnr;
+	uint32_t rttr;
+	uint32_t __reserved;
+	uint32_t rtsr;
+} RTC_registers;
+
+#ifndef RTC_pointer
+#define	RTC_pointer	((RTC_registers*) RTC_BASE)
+#endif
+
+#define	RTAR		RTC_pointer->rtar
+#define	RCNT		RTC_pointer->rcnr
+#define	RTTR		RTC_pointer->rttr
+#define	RTSR		RTC_pointer->rtsr
+
+#endif	/* SA11X0_RTC_H */
