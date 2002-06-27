614a4c5fd98a86ff3e2c099780e65ba8eac48ee8
Marcel Telka <marcel@telka.sk>
2002-06-27 08:09:09 +0000

    Added Clocks Manager Registers.
    git-svn-id: https://urjtag.svn.sourceforge.net/svnroot/urjtag/trunk@17 b68d4a1b-bc3d-0410-92ed-d4ac073336b7

---
+++ include/arm/pxa2x0/cm.h
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ *
+ * XScale PXA250/PXA210 Clocks Manager Registers
+ * Copyright (C) 2002 ETC s.r.o.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by Marcel Telka <marcel@telka.sk>, 2002.
+ *
+ * Documentation:
+ * [1] Intel Corporation, "Intel PXA250 and PXA210 Application Processors
+ *     Developer's Manual", February 2002, Order Number: 278522-001
+ *
+ */
+
+#ifndef	PXA2X0_CM_H
+#define	PXA2X0_CM_H
+
+#ifndef uint32_t
+typedef	unsigned int	uint32_t;
+#endif
+
+/* Clocks Manager Registers */
+
+#define	CM_BASE		0x41300000
+
+typedef volatile struct CM_registers {
+	uint32_t cccr;
+	uint32_t cken;
+	uint32_t oscc;
+} CM_registers;
+
+#ifndef CM_pointer
+#define	CM_pointer	((CM_registers*) CM_BASE)
+#endif
+
+#define	CCCR		CM_pointer->cccr
+#define	CKEN		CM_pointer->cken
+#define	OSCC		CM_pointer->oscc
+
+#endif	/* PXA2X0_CM_H */
