e601010666df9ef13c8af458bfb44d2eb9c81620
Marcel Telka <marcel@telka.sk>
2002-06-27 13:44:22 +0000

    Added LCD Controller Registers.
    git-svn-id: https://urjtag.svn.sourceforge.net/svnroot/urjtag/trunk@34 b68d4a1b-bc3d-0410-92ed-d4ac073336b7

---
+++ include/arm/sa11x0/lcd.h
@@ -0,0 +1,70 @@
+/*
+ * $Id$
+ *
+ * StrongARM SA-1110 LCD Controller Registers
+ * Copyright (C) 2002 ETC s.r.o.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by Marcel Telka <marcel@telka.sk>, 2002.
+ *
+ * Documentation:
+ * [1] Intel Corporation, "Intel StrongARM SA-1110 Microprocessor
+ *     Developer's Manual", October 2001, Order Number: 278240-004
+ * [2] Intel Corporation, "Intel StrongARM SA-1110 Microprocessor
+ *     Specification Update", December 2001, Order Number: 278259-023
+ *
+ */
+
+#ifndef	SA11X0_LCD_H
+#define	SA11X0_LCD_H
+
+#ifndef uint32_t
+typedef	unsigned int	uint32_t;
+#endif
+
+/* LCD Controller Registers */
+
+#define	LCD_BASE	0xB0100000
+
+typedef volatile struct LCD_registers {
+	uint32_t lccr0;
+	uint32_t lcsr;
+	uint32_t __reserved[2];
+	uint32_t dbar1;
+	uint32_t dcar1;
+	uint32_t dbar2;
+	uint32_t dcar2;
+	uint32_t lccr1;
+	uint32_t lccr2;
+	uint32_t lccr3;
+} LCD_registers;
+
+#ifndef LCD_pointer
+#define	LCD_pointer	((LCD_registers*) LCD_BASE)
+#endif
+
+#define	LCCR0		LCD_pointer->lccr0
+#define	LCSR		LCD_pointer->lcsr
+#define	DBAR!		LCD_pointer->dbar1
+#define	DCAR!		LCD_pointer->dcar1
+#define	DBAR2		LCD_pointer->dbar2
+#define	DCAR2		LCD_pointer->dcar2
+#define	LCCR1		LCD_pointer->lccr1
+#define	LCCR2		LCD_pointer->lccr2
+#define	LCCR3		LCD_pointer->lccr3
+
+#endif	/* SA11X0_LCD_H */
