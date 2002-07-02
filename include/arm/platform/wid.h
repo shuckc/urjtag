96d74e58db817db3cc7eb94af1d31c0a586d64a9
Marcel Telka <marcel@telka.sk>
2002-07-02 15:14:10 +0000

    Added initial platform specific declarations.
    git-svn-id: https://urjtag.svn.sourceforge.net/svnroot/urjtag/trunk@41 b68d4a1b-bc3d-0410-92ed-d4ac073336b7

---
+++ include/arm/platform/wid.h
@@ -0,0 +1,44 @@
+/*
+ * $Id$
+ *
+ * ETC WID specific declarations (SA-1110 based device)
+ * Copyright (C) 2002 ETC s.r.o.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by Marcel Telka <marcel@telka.sk>, 2002.
+ *
+ * Documentation:
+ * [1] Intel Corporation, "Intel StrongARM SA-1110 Microprocessor
+ *     Developer's Manual", October 2001, Order Number: 278240-004
+ * [2] Intel Corporation, "Intel StrongARM SA-1110 Microprocessor
+ *     Specification Update", December 2001, Order Number: 278259-023
+ *
+ */
+
+#ifndef	PLATFORM_WID_H
+#define	PLATFORM_WID_H
+
+/* WID board variations */
+#define	EI370	0
+#define	EP002	1
+#define	EP202	2
+
+#define	PPCR_CCF_CLOCK	PPCR_CCF_206_4
+
+#define	WID_GPIO_BCR_MASK	bit(17)
+
+#endif /* PLATFORM_WID_H */
