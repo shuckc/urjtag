09884928dd35734046bec59a583822793a8abc28
Marcel Telka <marcel@telka.sk>
2002-10-24 15:51:02 +0000

    Added bus operations for SA1110.
    git-svn-id: https://urjtag.svn.sourceforge.net/svnroot/urjtag/trunk@209 b68d4a1b-bc3d-0410-92ed-d4ac073336b7

---
+++ jtag/src/pxa250.h
@@ -0,0 +1,35 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2002 ETC s.r.o.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by Marcel Telka <marcel@telka.sk>, 2002.
+ *
+ */
+
+#ifndef PXA250_H
+#define	PXA250_H
+
+#include <stdint.h>
+
+#include "part.h"
+
+uint32_t pxa250_bus_read( parts *ps, uint32_t adr );
+void pxa250_bus_write( parts *ps, uint32_t adr, uint32_t data );
+
+#endif /* PXA250_H */
---
+++ jtag/src/sa1110.h
@@ -0,0 +1,35 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2002 ETC s.r.o.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by Marcel Telka <marcel@telka.sk>, 2002.
+ *
+ */
+
+#ifndef SA1110_H
+#define	SA1110_H
+
+#include <stdint.h>
+
+#include "part.h"
+
+uint32_t sa1110_bus_read( parts *ps, uint32_t adr );
+void sa1110_bus_write( parts *ps, uint32_t adr, uint32_t data );
+
+#endif /* SA1110_H */
