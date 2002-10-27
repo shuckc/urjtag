54eb53c099f90dbd6aa28401f867513010746924
Marcel Telka <marcel@telka.sk>
2002-10-27 14:59:36 +0000

    Ported to cygwin/newlib (intX_t are defined in sys/types.h).
    git-svn-id: https://urjtag.svn.sourceforge.net/svnroot/urjtag/trunk@226 b68d4a1b-bc3d-0410-92ed-d4ac073336b7

--- include/stdint.h
+++ include/stdint.h
@@ -24,13 +24,17 @@
 #include <stddef.h>
 
 /* 7.18.1.1  Exact-width integer types */
+#if defined(__CYGWIN__)
+#include <sys/types.h>
+#else
 typedef signed char int8_t;
-typedef unsigned char   uint8_t;
 typedef short  int16_t;
-typedef unsigned short  uint16_t;
 typedef int  int32_t;
-typedef unsigned   uint32_t;
 typedef long long  int64_t;
+#endif
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned   uint32_t;
 typedef unsigned long long   uint64_t;
 
 /* 7.18.1.2  Minimum-width integer types */
