df3728c6080ab2ae749fe5e4393298b21f364f88
Marcel Telka <marcel@telka.sk>
2002-10-30 09:01:29 +0000

    Ported to WinCE (removed 64-bit integer types).
    git-svn-id: https://urjtag.svn.sourceforge.net/svnroot/urjtag/trunk@240 b68d4a1b-bc3d-0410-92ed-d4ac073336b7

--- include/stdint.h
+++ include/stdint.h
@@ -30,12 +30,16 @@
 typedef signed char int8_t;
 typedef short  int16_t;
 typedef int  int32_t;
+#if !defined(UNDER_CE)
 typedef long long  int64_t;
-#endif
+#endif /* !defined(UNDER_CE) */
+#endif /* defined(__CYGWIN__) */
 typedef unsigned char   uint8_t;
 typedef unsigned short  uint16_t;
 typedef unsigned   uint32_t;
+#if !defined(UNDER_CE)
 typedef unsigned long long   uint64_t;
+#endif /* !defined(UNDER_CE) */
 
 /* 7.18.1.2  Minimum-width integer types */
 typedef signed char int_least8_t;
@@ -44,8 +48,10 @@ typedef short  int_least16_t;
 typedef unsigned short  uint_least16_t;
 typedef int  int_least32_t;
 typedef unsigned   uint_least32_t;
+#if !defined(UNDER_CE)
 typedef long long  int_least64_t;
 typedef unsigned long long   uint_least64_t;
+#endif /* !defined(UNDER_CE) */
 
 /*  7.18.1.3  Fastest minimum-width integer types 
  *  Not actually guaranteed to be fastest for all purposes
@@ -57,16 +63,20 @@ typedef short  int_fast16_t;
 typedef unsigned short  uint_fast16_t;
 typedef int  int_fast32_t;
 typedef unsigned  int  uint_fast32_t;
+#if !defined(UNDER_CE)
 typedef long long  int_fast64_t;
 typedef unsigned long long   uint_fast64_t;
+#endif /* !defined(UNDER_CE) */
 
 /* 7.18.1.4  Integer types capable of holding object pointers */
 typedef int intptr_t;
 typedef unsigned uintptr_t;
 
 /* 7.18.1.5  Greatest-width integer types */
+#if !defined(UNDER_CE)
 typedef long long  intmax_t;
 typedef unsigned long long   uintmax_t;
+#endif /* !defined(UNDER_CE) */
 
 /* 7.18.2  Limits of specified-width integer types */
 #if !defined ( __cplusplus) || defined (__STDC_LIMIT_MACROS)
